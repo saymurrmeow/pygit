c43822b5aa48a06fbbe337d5f544095278a4d032
saymurrmeow <r.savinovskii@gmail.com>
2023-01-04 22:37:41 +0300

    setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name = 'pygit',
+    version = '1.0',
+    packages = ['pygit'],
+    entry_points = {
+       'console_scripts' : [
+       'pygit = pygit.cli:main'
+       ]
+   })
